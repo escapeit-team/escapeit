a94296238f77d852ca702e79381135749bba18e8
Manuel Marcelino <likeboss520@gmail.com>
2019-08-08 20:39:52 +0100

    Update

--- src/LevelMenu.js
+++ src/LevelMenu.js
@@ -4,15 +4,17 @@ Ball.LevelMenu.prototype = {
 		this.add.sprite(0, 0, 'screen-bg');
 		this.level1Button = this.add.button(50, 50, 'lvlonebutton', this.level1, this, 2, 0, 1);
 		this.level1Button.input.useHandCursor = true;
-		this.level2Button = this.add.button(120 , 50, 'lvltwobutton', this.level2, this, 2, 0, 1);
-		this.level2Button.input.useHandCursor = true;
-		
-		// button to "read the article"
+		if(levelonecompleted === true) {
+			this.level2Button = this.add.button(120, 50, 'lvltwobutton', this.level2, this, 2, 0, 1);
+			this.level2Button.input.useHandCursor = true;
+		}
 	},
 	level1: function() {
 			this.game.state.start('Level1');
 	},
 	level2: function() {
-			this.game.state.start('Level2');
+			if(levelonecompleted === true) {
+				this.game.state.start('Level2');
+			}
 	}
 };
