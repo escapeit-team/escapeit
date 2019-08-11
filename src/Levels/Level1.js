dd34a5631346c70a8be2c5d1462cda7769dcd6a9
Vasco Frazão <vaffsco@hotmail.com>
2019-08-11 22:45:49 +0100

    Update

--- src/Levels/Level1.js
+++ src/Levels/Level1.js
@@ -49,14 +49,6 @@ Ball.Level1.prototype = {
 		this.spikeGroup.physicsBodyType = Phaser.Physics.ARCADE;
 		this.spike1 = this.spikeGroup.create(200, 500-18, 'spikes');
 		this.spikeGroup.setAll('body.immovable', true);
-
-
-
-		this.reboundGroup = this.add.group();
-		this.reboundGroup.enableBody = true;
-		this.reboundGroup.physicsBodyType = Phaser.Physics.ARCADE;
-		this.rebound1 = this.reboundGroup.create(0, 500-18, 'rebound');
-		this.reboundGroup.setAll('body.immovable', true);
 		
 
 
@@ -87,7 +79,7 @@ Ball.Level1.prototype = {
 		
 		this.borderH = this.borderGroup.create(0, this.game.world.height - 1, 'border-horizontal');
 		this.borderH.scale.setTo(3,1);
-		//this.borderGroup.create(0, Ball._HEIGHT-2, 'border-horizontal');
+		this.borderGroup.create(0, 0, 'border-horizontal');
 
 		this.borderV = this.borderGroup.create(0, 0, 'border-vertical');
 		this.borderV.scale.setTo(1,2.01);
@@ -378,7 +370,7 @@ Ball.Level1.prototype = {
 
 		this.physics.arcade.collide(this.ball, this.wallGroup, this.wallCollision, null, this);
 		this.physics.arcade.collide(this.ball, this.spikeGroup, this.ballLives, null, this);
-		this.physics.arcade.collide(this.ball, this.reboundGroup, this.reboundExtra, null, this);
+
 			
 		this.physics.arcade.overlap(this.ball, this.button, this.collectButton, null, this);
 		this.physics.arcade.overlap(this.ball, this.speedUp, this.collectSpeedUp, null, this);
@@ -400,17 +392,6 @@ Ball.Level1.prototype = {
 	},
 
 
-	reboundExtra: function() {
-		if (		this.physics.arcade.collide(this.ball, this.reboundGroup, this.reboundExtra, null, this)
-		){
-			this.ball.body.bounce.set(3, 3);}
-			else{
-				this.ball.body.bounce.set(.3, .3);
-
-			}
-
-
-	},
 	
 	collectButton: function() {
 		this.button.kill();
