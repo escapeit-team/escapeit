d17d446c58885b8c86ce851ba88688504c19a4c6
Vasco Frazão <vaffsco@hotmail.com>
2019-09-09 14:10:14 +0100

    u

--- dist/src/Levels/Level1.js
+++ dist/src/Levels/Level1.js
@@ -21,7 +21,7 @@ Ball.Level1.prototype = {
 		this.maxLevels = 3;
 		this.movementForce = 10;
 		this.ballStartPos = { x: 40, y: 32 };
-		this.lives = 3;
+		this.lives = 4;
 
 
 		this.hole = this.add.sprite(32*13, 64, 'hole');
@@ -193,9 +193,8 @@ Ball.Level1.prototype = {
 	},
 	managePause: function() {
 		this.game.paused = true;
-		this.pausedText = this.add.text(200, 250, "Game paused,\ntap anywhere to continue.", this.fontMessage);
+		this.pausedText = this.game.add.text(100, 250, "Game paused,\ntap anywhere to continue.", this.fontMessage);
 		this.pausedText.fixedToCamera = true;
-		pausedText.anchor.set(0.5);
 		this.input.onDown.add(function(){
 			pausedText.kill();
 			this.game.paused = false;
